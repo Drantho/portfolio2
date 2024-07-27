449fa66de61670cd0ee63daa10222e478408cb06
Anthony Mitchell <drantho@gmail.com>
2024-07-27 16:46:29 -0500

    accessibility updates

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,11 +5,12 @@ import styles from "@/styles/Button.module.css";
 type ButtonProps = {
   children: ReactNode;
   href: string;
+  alt: string;
   style?: CSSProperties;
 };
 
 const Button = (props: ButtonProps) => {
-    const { children, href, style } = props;
+    const { children, href, style, alt } = props;
   return (
     <a href={href} style={style} className={styles.button}>{children}</a>
   )
